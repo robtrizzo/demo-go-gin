7ea68cde436d59c5c741e543f2e2937133506b80
Robert Rizzo <robtrizzo@gmail.com>
2023-12-29 15:34:14 -0500

    render function based on accept header

--- handlers/article.go
+++ handlers/article.go
@@ -13,18 +13,11 @@ func GetArticle(c *gin.Context) {
 	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
 		// Check if the article exists
 		if article, err := models.GetArticleByID(articleID); err == nil {
-			// Call the HTML method of the Context to render a template
-			c.HTML(
-				// Set the HTTP status to be 200 (OK)
-				http.StatusOK,
-				// Use the article.html template
-				"article.html",
-				// Pass the data that the page uses
-				gin.H{
-					"title":   article.Title,
-					"payload": article,
-				},
-			)
+			// Call the render function with the name of the template to render
+			render(c, gin.H{
+				"title":   article.Title,
+				"payload": article,
+			}, "article.html")
 		} else {
 			// If the article is not found, abort with an error
 			c.AbortWithError(http.StatusNotFound, err)
--- handlers/index.go
+++ handlers/index.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/robtrizzo/semaphore-demo-go-gin/models"
 )
@@ -10,16 +8,9 @@ import (
 func ShowIndexPage(c *gin.Context) {
 	articles := models.GetAllArticles()
 
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HTTP status to 200
-		http.StatusOK,
-		// Use the index.html template
-		"index.html",
-		// Pass the data that the page uses
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	// Call the render function with the name of the template to render
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles,
+	}, "index.html")
 }
